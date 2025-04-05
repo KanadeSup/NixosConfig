6dbf39fc339dfe69280c698e9bc71c39d287f059
Kanade <KanadeSupreme@gmail.com>
2025-04-05 14:22:40 +0700

    Feature: Add language server for neovim

--- nix/modules/core/packages.nix
+++ nix/modules/core/packages.nix
@@ -1,24 +1,34 @@
 { pkgs, ... }:
 
 with pkgs; [
-  bibata-cursors
-  kitty
-  unzip
-  neovim
-  sxhkd
-  bat
-  procs
-  zoxide
-  eza
-  delta
-  git
-  duf
-  dust
-  ripgrep
-  fd
-  silver-searcher
-  fzf
-  tldr
-  bottom
-  nerd-fonts.fira-code
+   bibata-cursors
+   kitty
+   unzip
+   neovim
+   sxhkd
+   bat
+   procs
+   zoxide
+   eza
+   delta
+   git
+   duf
+   dust
+   ripgrep
+   fd
+   silver-searcher
+   fzf
+   tldr
+   bottom
+   nerd-fonts.fira-code
+   clang_19
+   zellij
+   # Language server protocol
+   lua-language-server
+   typescript
+   typescript-language-server
+   rust-analyzer
+   tailwindcss-language-server
+   vue-language-server
+
 ]
